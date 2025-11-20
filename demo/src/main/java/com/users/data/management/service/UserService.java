307d9ad099fe0f9e61028f2d946fa68e013acc5a
Rajkumar Yadav <89531059+itsyadavRajkumar@users.noreply.github.com>
2025-11-20 10:49:05 +0530

    Delete demo directory

--- demo/src/main/java/com/users/data/management/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.test.service;
-
-import com.test.dto.UserDto;
-
-public interface UserService {
-    UserDto createUser(UserDto userDto);
-    UserDto getUserData(Long id);
-    UserDto updateUserData(UserDto userDto);
-}
